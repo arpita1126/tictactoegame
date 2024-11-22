d9ce5c395662b155079d7566d9017dd3c8fed402
arpita <arpita.2226csit1071@kiet.edu>
2024-11-22 14:38:41 +0530

    Tictactoe using angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { TictactoeComponent } from './tictactoe/tictactoe.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TodoComponent, TictactoeComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/tictactoe/tictactoe.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tictactoe',
+  templateUrl: './tictactoe.component.html',
+  styleUrl: './tictactoe.component.css'
+})
+export class TictactoeComponent {
+  cells: string[] = Array(10).fill(null);
+  playerSymbol: string = 'X';
+
+  onGridCellClick(index: number): void {
+    this.cells[index]=this.playerSymbol;  
+    if(this.playerSymbol=='X'){this.playerSymbol="O";}
+    else{this.playerSymbol='X';}
+  }
+
+
+
+
+
+}
